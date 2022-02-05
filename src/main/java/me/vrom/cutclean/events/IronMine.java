6136445957507b3c80ea26c12e2ded1de420b4f9
Christopher Nethercott <ccnethercott@gmail.com>
2022-02-05 15:31:07 +0000

    fix: reworked and simplified cutclean main logic - this should be abstracted out to a main class

--- src/main/java/me/vrom/cutclean/events/IronMine.java
+++ src/main/java/me/vrom/cutclean/events/IronMine.java
@@ -18,43 +18,23 @@ public class IronMine implements Listener {
     @EventHandler
     public void onBreak(BlockBreakEvent e) {
         if (e.getBlock().getType().equals(Material.IRON_ORE)) {
+            e.getBlock().setType(Material.AIR);
+
             //Check if the item in hand is a pickaxe has fortune
             if (e.getPlayer().getInventory().getItemInHand().getEnchantments().containsKey(Enchantment.LOOT_BONUS_BLOCKS)) {
                 //Check if the player has permission cutclean.ironmine
                 if (e.getPlayer().hasPermission("cutclean.ironmine")) {
-                    //Check if the fortune level is 1
-                    if (e.getPlayer().getInventory().getItemInHand().getEnchantments().get(Enchantment.LOOT_BONUS_BLOCKS) == 1) {
-                        //Make the iron ore drop 2 iron ingots
-                        e.getBlock().setType(Material.AIR);
-                        //Drop 2 iron ingots
-                        e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), new ItemStack(Material.IRON_INGOT, plugin.getConfig().getInt("iron.1")));
-                    }
-                    //Check if the fortune level is 2
-                    if (e.getPlayer().getInventory().getItemInHand().getEnchantments().get(Enchantment.LOOT_BONUS_BLOCKS) == 2) {
-                        //Make the iron ore drop 3 iron ingots
-                        e.getBlock().setType(Material.AIR);
-                        //Drop 3 iron ingots
-                        e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), new ItemStack(Material.IRON_INGOT, plugin.getConfig().getInt("iron.2")));
-                    }
-                    //Check if the fortune level is 3
-                    if (e.getPlayer().getInventory().getItemInHand().getEnchantments().get(Enchantment.LOOT_BONUS_BLOCKS) == 3) {
-                        //Make the iron ore drop 4 iron ingots
-                        e.getBlock().setType(Material.AIR);
-                        //Drop 4 iron ingots
-                        e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), new ItemStack(Material.IRON_INGOT, plugin.getConfig().getInt("iron.3")));
-                    }
-                    //Check if fortune level is more than 3
-                    if (e.getPlayer().getInventory().getItemInHand().getEnchantments().get(Enchantment.LOOT_BONUS_BLOCKS) > 3) {
-                        //Make the iron ore drop 5 iron ingots
-                        e.getBlock().setType(Material.AIR);
-                        //Drop 5 iron ingots
+
+                    int enchantmentLevel = e.getPlayer().getInventory().getItemInHand().getEnchantmentLevel(Enchantment.LOOT_BONUS_BLOCKS);
+
+                    if (enchantmentLevel <= 3) {
+                        e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), new ItemStack(Material.IRON_INGOT, plugin.getConfig().getInt("iron." + enchantmentLevel)));
+                    } else {
                         e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), new ItemStack(Material.IRON_INGOT, plugin.getConfig().getInt("iron.4")));
                     }
+
                 }
             } else {
-                //Make the iron ore drop 1 iron ingot
-                e.getBlock().setType(Material.AIR);
-                //Drop iron ingot with amount from config
                 e.getBlock().getWorld().dropItemNaturally(e.getBlock().getLocation(), new ItemStack(Material.IRON_INGOT, plugin.getConfig().getInt("iron.normal")));
             }
         }
